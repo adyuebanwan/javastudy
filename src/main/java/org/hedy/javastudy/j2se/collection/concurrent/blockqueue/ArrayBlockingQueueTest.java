dc7a455fd63b66f9658d21691de2c17d6b60e790
hedongyu <554151811@qq.com>
2015-10-29 18:30:45 +0800

    1

--- src/main/java/org/hedy/javastudy/j2se/collection/concurrent/blockqueue/ArrayBlockingQueueTest.java
+++ src/main/java/org/hedy/javastudy/j2se/collection/concurrent/blockqueue/ArrayBlockingQueueTest.java
@@ -7,7 +7,7 @@ import java.util.concurrent.TimeUnit;
 /**
  * Created by hzhedongyu on 2015/10/29.
  */
-public class BlockingQueueTest {
+public class ArrayBlockingQueueTest {
     public static void main(String[] args) {
         BlockingQueue<String> queue = new ArrayBlockingQueue<String>(1);
         new Thread(new Adder<String>(queue)).start();
